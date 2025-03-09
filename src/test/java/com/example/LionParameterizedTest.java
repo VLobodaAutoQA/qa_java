27a3cf7afb9d38ba7de8629a1965ff20ce7886b7
Vladislav Loboda <68188379+VLobodaAutoQA@users.noreply.github.com>
2025-03-09 17:47:42 +0300

    Init Mock in LionParameterizedTest.java, Add new Class LionExceptionTest

--- src/test/java/com/example/LionParameterizedTest.java
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -1,9 +1,11 @@
 package com.example;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -13,14 +15,16 @@ import static org.junit.Assert.*;
 @RunWith(Parameterized.class)
 public class LionParameterizedTest {
 
-    @Parameterized.Parameter(0)
-    public String sex;
-
-    @Parameterized.Parameter(1)
-    public boolean expectedHasMane;
+    private final String sex;
+    private final boolean expectedHasMane;
 
     @Mock
     private Feline feline;
+
+    public LionParameterizedTest(String sex, boolean expectedHasMane) {
+        this.sex = sex;
+        this.expectedHasMane = expectedHasMane;
+    }
 
     @Parameterized.Parameters(name = "Test with sex={0}, expected hasMane={1}")
     public static Collection<Object[]> data() {
@@ -28,6 +32,11 @@ public class LionParameterizedTest {
                 {"Самец", true},
                 {"Самка", false}
         });
+    }
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
@@ -38,6 +47,6 @@ public class LionParameterizedTest {
 
     @Test(expected = Exception.class)
     public void testInvalidSexThrowsException() throws Exception {
-        new Lion("Некорректное значение", feline);
+        new Lion("Используйте допустимые значения пола животного - самец или самка", feline);
     }
 }
